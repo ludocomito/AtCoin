2156690c58b12c89f50357c4e77e148a41f84464
luisblitz22 <97792302+luisblitz22@users.noreply.github.com>
2022-05-12 10:50:14 +0200

    Add files via upload

--- php/Registrazione.php
+++ php/Registrazione.php
@@ -92,7 +92,7 @@
 			<div class="alert alert-success d-flex align-items-center" role="alert">
             	<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
             	<div>
-                	<span> &nbsp La registrazione dell'utente : " " è avvenuta con successo !</span>
+                	<span> &nbsp La registrazione dell'utente è avvenuta con successo !</span>
             	</div>
        		</div>
 			
